cd121fbfd8611c829787bc0b2706ad64c93e0376
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2022-04-24 13:40:06 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('div.gallery');
+
+galleryItems.map(el => {
+
+    const galleryLink = document.createElement('a');
+    const galleryItem = document.createElement('div');
+    const galleryImage = document.createElement('img');
+
+
+    galleryLink.classList.add('gallery__link');
+    galleryItem.classList.add('gallery__item');
+
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = el.preview;
+
+
+    galleryItem.append(galleryImage);
+    galleryLink.append(galleryItem);
+    
+    gallery.append(galleryLink);
+    return gallery;
+})
+
+
+
+
+
+
+
+
+
+
+
