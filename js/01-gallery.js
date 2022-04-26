b7cf2f1de8e8d0156fd88ba02044138b6fabf9b9
Kostiantyn Honcharov <goncharov.kostiantyn@gmail.com>
2022-04-26 13:08:43 +0300

    Some change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
 
 const gallery = document.querySelector('div.gallery');
+
 
 galleryItems.map(el => {
 
@@ -11,20 +13,42 @@ galleryItems.map(el => {
     const galleryItem = document.createElement('div');
     const galleryImage = document.createElement('img');
 
-
     galleryLink.classList.add('gallery__link');
     galleryItem.classList.add('gallery__item');
 
     galleryImage.classList.add('gallery__image');
     galleryImage.src = el.preview;
-
+    galleryImage.dataset.sourse = el.original;
+    galleryImage.alt = el.description;
 
     galleryItem.append(galleryImage);
     galleryLink.append(galleryItem);
     
     gallery.append(galleryLink);
     return gallery;
-})
+});
+
+
+gallery.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    basicLightbox.create(`
+        <img src=${e.target.dataset.sourse} width="800" height="600">
+`, {
+        onShow: (e) => {
+        window.addEventListener("keydown", (event) => {
+            if (event.code === "Escape") { 
+                e.close(); 
+                } 
+            })
+        }
+    }).show();
+    
+});
+   
 
 
 
